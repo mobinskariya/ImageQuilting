a75fa6e4283814a9f8bcfe6381271dc25dc7110a
Mobin Skariya <mobinskariya@gmail.com>
2015-11-07 21:35:49 +0530

    previous and current images blend peacefully

--- ImageQuilting/src/ImgQltService.cu
+++ ImageQuilting/src/ImgQltService.cu
@@ -13,7 +13,9 @@
 #include<time.h>
 #include <sys/time.h>
 #include <unistd.h>
-
+#include<map>
+#include<list>
+#include<vector>
 
 #define SAMPLE_SIZE 20
 #define OVERLAP_SIZE 5
@@ -22,6 +24,7 @@
 
 using std::cout;
 using std::endl;
+using namespace std;
 
 std::vector<cv::Mat> createImageList(cv::Mat& hSrc) {
 	int x_size = hSrc.rows;
@@ -46,21 +49,6 @@ int computeSSD(cv::Mat& overlap1, cv::Mat& overlap2) {
 	cv::Mat diff;
 	cv::absdiff(gray1, gray2, diff);
 	sum = cv::sum(diff)[0];
-
-	/*for (int i = 0; i < overlap1.rows; i++) {
-		for (int j = 0; j < overlap1.cols; j++) {
-			uchar val1 = gray1.at<uchar>(i,j);// getPixelValue(overlap1.at<cv::Vec3b>(i,j));
-			//cout << "val1: " << val1 << endl;
-			uchar val2 = gray2.at<uchar>(i,j);//getPixelValue(overlap2.at<cv::Vec3b>(i,j));
-			//cout << "val2: " << val2 << endl;
-			sum += std::sqrt(std::pow((val1 - val2), 2 ));
-			//cout << "sum " << sum << endl;
-		}
-		//	sum += ((oi1[j] - oi2[j]) ^ 2) ^ 0.5;
-	}*/
-
-	//cout << "overlap2.rows " << overlap1.rows << endl;
-	//cout << "overlap2.cols " << overlap1.cols << endl;
 
 	return sum;
 }
@@ -146,35 +134,192 @@ void placeImg(int row, int col, cv::Mat& tile, cv::Mat& lImg) {
 	tile.copyTo(lImg(cv::Range(x1, x2), cv::Range(y1, y2)));
 }
 
-void findVerticalMinPath(cv::Mat& currImg, cv::Mat& prevImg) {
+double findVerticalMinCost(int i, int j, cv::Mat& diff, map<pair<int, int >, double>& costMap, map<pair<int, int >, vector<pair<int, int > > >& pathMap) {
+	double cost, costjp1, costj, costjm1, minimum;
+	int cols = diff.cols;
+	vector<pair<int, int > > list1, list2;
+	list1.push_back(make_pair(i,j));
+	double test = costMap[make_pair(2,2)];
+	if( i > 0) {
+		if(j > 0 && j < cols - 1) {
+			costjp1 = costMap[make_pair(i-1,j+1)] != -1 ? costMap[make_pair(i-1,j+1)] : findVerticalMinCost(i-1,j+1, diff,costMap, pathMap);
+			costj = costMap[make_pair(i-1,j)] != -1 ? costMap[make_pair(i-1,j)] : findVerticalMinCost(i-1,j, diff,costMap, pathMap);
+			costjm1 = costMap[make_pair(i-1,j-1)] != -1 ? costMap[make_pair(i-1,j-1)] : findVerticalMinCost(i-1,j-1, diff,costMap, pathMap);
+			double costarr[] = {costjp1,costj,costjm1};
+			minimum = *min_element(costarr,costarr+3);
+			if(minimum == costjp1) {
+				list2 = pathMap[make_pair(i-1,j+1)];
+			} else if (minimum == costjm1) {
+				list2 = pathMap[make_pair(i-1,j-1)];
+			} else {
+				list2 = pathMap[make_pair(i-1,j)];
+			}
+		} else if (j == 0) {
+			costjp1 = costMap[make_pair(i-1,j+1)] != -1 ? costMap[make_pair(i-1,j+1)] : findVerticalMinCost(i-1,j+1, diff,costMap, pathMap);
+			costj = costMap[make_pair(i-1,j)] != -1 ? costMap[make_pair(i-1,j)] : findVerticalMinCost(i-1,j, diff,costMap, pathMap);
+			double costarr[] = {costjp1,costj};
+			minimum = *min_element(costarr,costarr+2);
+			if(minimum == costjp1) {
+				list2 = pathMap[make_pair(i-1,j+1)];
+			} else {
+				list2 = pathMap[make_pair(i-1,j)];
+			}
+		} else if (j == cols - 1) {
+			costj = costMap[make_pair(i-1,j)] != -1 ? costMap[make_pair(i-1,j)] : findVerticalMinCost(i-1,j, diff,costMap, pathMap);
+			costjm1 = costMap[make_pair(i-1,j-1)] != -1 ? costMap[make_pair(i-1,j-1)] : findVerticalMinCost(i-1,j-1, diff,costMap, pathMap);
+			double costarr[] = {costjp1,costj};
+			minimum = *min_element(costarr,costarr+2);
+			if(minimum == costjm1) {
+				list2 = pathMap[make_pair(i-1,j-1)];
+			} else {
+				list2 = pathMap[make_pair(i-1,j)];
+			}
+		}
+		list1.insert(list1.end(), list2.begin(), list2.end());
+		cost = minimum + static_cast<double>( diff.at<uchar>(i,j) );
+		costMap[make_pair(i,j)] = cost;
+		pathMap[make_pair(i,j)] = list1;
+	} else {
+		cost = static_cast<double>( diff.at<uchar>(i,j) );
+		costMap[make_pair(i,j)] = cost;
+		pathMap[make_pair(i,j)].push_back(make_pair(i,j));
+	}
+	return cost;
+}
+
+vector<pair<int,int> > findVerticalMinPath(cv::Mat& diff) {
+
+	map<pair<int, int >, double> costMap;
+
+	for(int i = 0; i < diff.rows; i++) {
+		for(int j =0; j < diff.cols; j++) {
+			costMap[make_pair(i,j)] = -1;
+		}
+	}
+
+	map<pair<int, int >, vector<pair<int, int > > > pathMap;
+
+	vector<double> mincost;
+	vector<vector<pair<int,int> > > minpaths;
+
+	int lastrow = diff.rows - 1;
+	for(int i = 0 ; i < diff.cols; i++) {
+		mincost.push_back(findVerticalMinCost(lastrow,i, diff,costMap, pathMap));
+		minpaths.push_back(pathMap[make_pair(lastrow,i)]);
+	}
+
+	int minidx = find(minpaths.begin(), minpaths.end(), *min_element(minpaths.begin(),minpaths.end())) - minpaths.begin();
+
+	vector<pair<int,int> > minpath = minpaths[minidx];
+
+	for(int i = 0; i < minpath.size(); i++) {
+		cout << minpath[i].first << " : " << minpath[i].second << endl;
+	}
+	return minpath;
+}
+
+vector<pair<int,int> > findHorizontalMinPath(cv::Mat& diff) {
+
+	map<pair<int, int >, double> costMap;
+
+	for(int j = 0; j < diff.cols; j++) {
+		for(int i = 0; i < diff.rows; i++) {
+			costMap[make_pair(i,j)] = -1;
+		}
+	}
+
+	map<pair<int, int >, vector<pair<int, int > > > pathMap;
+
+	vector<double> mincost;
+	vector<vector<pair<int,int> > > minpaths;
+
+	int lastcol = diff.cols - 1;
+	for(int i = 0 ; i < diff.rows; i++) {
+		//mincost.push_back(findHorizontalMinCost(i,lastcol, diff,costMap, pathMap));
+		minpaths.push_back(pathMap[make_pair(i,lastcol)]);
+	}
+
+	int minidx = find(minpaths.begin(), minpaths.end(), *min_element(minpaths.begin(),minpaths.end())) - minpaths.begin();
+
+	vector<pair<int,int> > minpath = minpaths[minidx];
+
+	for(int i = 0; i < minpath.size(); i++) {
+		cout << minpath[i].first << " : " << minpath[i].second << endl;
+	}
+	return minpath;
+}
+
+void blendVertically(cv::Mat& currImg, cv::Mat& prevImg) {
 	cv::Mat overlap1 = prevImg(cv::Range(0,prevImg.rows),cv::Range(prevImg.cols-OVERLAP_SIZE,prevImg.cols));
 	cv::Mat overlap2 = currImg(cv::Range(0,currImg.rows),cv::Range(0,OVERLAP_SIZE));
 	cv::Mat gray1, gray2;
 
 	cv::cvtColor(overlap1, gray1, cv::COLOR_BGR2GRAY);
 	cv::cvtColor(overlap2, gray2, cv::COLOR_BGR2GRAY);
-	cv::Mat error(overlap1.rows, overlap1.cols, CV_8UC1);
+	cv::Mat diff;
 
-	cv::Mat diff = gray1 - gray2;
-	cv::Mat pow;
-	cv::multiply(diff, diff, pow);
-	cv::sqrt(pow,error);
+	cv::absdiff(gray1, gray2, diff);
 
-	for (int i = 0; i < overlap1.rows; i++) {
-		for (int j = 0; j < overlap1.cols; j++) {
-			uchar val1 = gray1.at<uchar>(i,j);// getPixelValue(overlap1.at<cv::Vec3b>(i,j));
-			uchar val2 = gray2.at<uchar>(i,j);//getPixelValue(overlap2.at<cv::Vec3b>(i,j));
-			//error[] = std::sqrt(std::pow((val1 - val2), 2 ));
+	vector<pair<int,int> > verticalPath;
+
+	//cv::imwrite("testimg.jpg",currImg);
+	cout << "diff\n" << diff << endl;
+	verticalPath = findVerticalMinPath(diff);
+
+	for(int i = 0; i < verticalPath.size(); i++) {
+		int rowIdx = verticalPath[i].first;
+		int colIdx = verticalPath[i].second;
+		for(int j = 0; j < colIdx; j++) {
+			cv::Vec3b& currPixel = overlap2.at<cv::Vec3b>(rowIdx,j);
+			cv::Vec3b& prePixel = overlap1.at<cv::Vec3b>(rowIdx,j);
+			currPixel[0] = prePixel[0];
+			currPixel[1] = prePixel[1];
+			currPixel[2] = prePixel[2];
 		}
 	}
+}
 
+void blendHorizontally(cv::Mat& currImg, cv::Mat& topImg) {
+	cv::Mat overlap1 = topImg(cv::Range(topImg.rows-OVERLAP_SIZE,topImg.rows),cv::Range(0,topImg.cols));
+	cv::Mat overlap2 = currImg(cv::Range(0,OVERLAP_SIZE),cv::Range(0,topImg.cols));
+	cv::Mat gray1, gray2;
+
+	cv::cvtColor(overlap1, gray1, cv::COLOR_BGR2GRAY);
+	cv::cvtColor(overlap2, gray2, cv::COLOR_BGR2GRAY);
+	cv::Mat diff;
+
+	cv::absdiff(gray1, gray2, diff);
+
+	vector<pair<int,int> > horizontalPath;
+
+	//cv::imwrite("testimg.jpg",currImg);
+	cout << "diff\n" << diff << endl;
+	horizontalPath = findHorizontalMinPath(diff);
+
+	for(int i = 0; i < horizontalPath.size(); i++) {
+		int rowIdx = horizontalPath[i].first;
+		int colIdx = horizontalPath[i].second;
+		for(int j = 0; j < rowIdx; j++) {
+			cv::Vec3b& currPixel = overlap2.at<cv::Vec3b>(j,colIdx);
+			cv::Vec3b& prePixel = overlap1.at<cv::Vec3b>(j,colIdx);
+			currPixel[0] = prePixel[0];
+			currPixel[1] = prePixel[1];
+			currPixel[2] = prePixel[2];
+		}
+	}
 }
 
 void blendImage(cv::Mat& currImg, cv::Mat& prevImg, cv::Mat& topImg) {
-
+	if (prevImg.dims > 0) {
+		blendVertically(currImg, prevImg);
+	}
+	if(topImg.dims > 0) {
+		//blendHorizontally(currImg, topImg);
+	}
 }
 
 void imageQuilting(cv::Mat& hSrc, cv::Mat& hDst) {
+
 
 
 	std::vector<cv::Mat> imglist = createImageList(hSrc);
@@ -185,21 +330,31 @@ void imageQuilting(cv::Mat& hSrc, cv::Mat& hDst) {
 	for(int i = 0; i < nx; i++ ) {
 		for(int j = 0; j < ny; j++) {
 
-			//cout << "i , j : " << i << " : " << j << endl;
+			cout << "i , j : " << i << " : " << j << endl;
 			cv::Mat prevImg = getPreviousImg(i, j, hDst);
 
 			cv::Mat topImg = getTopImg(i, j, hDst);
 
 			clock_t start = clock();
 			cv::Mat currImg = getMinSSDImg(prevImg, topImg, imglist);
+
+			//cv::imwrite("testimg.jpg",currImg);
 			clock_t end = clock();
 			double elapsed_secs = double(end - start) * 1000 / CLOCKS_PER_SEC;
 
 			cout << "Execution time:" << elapsed_secs <<"  ms"<< endl;
 
-			//cout << currImg << endl;
-			placeImg(i, j, currImg, hDst);
+			//placeImg(i, j, currImg, hDst);
 
+			//cv::imwrite("testimg.jpg",hDst);
+			cv::Mat currImgCopy;
+			currImg.copyTo(currImgCopy);
+			blendImage(currImgCopy,prevImg,topImg);
+			//cout << currImg << endl;
+			placeImg(i, j, currImgCopy, hDst);
+			//cv::imshow("hDst", hDst);
+			//cv::waitKey();
+			//cv::imwrite("testimg.jpg",hDst);
 		}
 	}
 
